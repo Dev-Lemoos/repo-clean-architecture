3d7dd33df59b7abc31da16bcc68e53f2772c3226
Gustavo Lemos <gu.leminhos@outlook.com>
2022-12-08 07:55:24 -0300

    Entidade Category seguindo os padrãos da clean arq

--- curso_cleanarq/CleanArchMvc/CleanArchMvc.Domain/Entities/Category.cs
+++ curso_cleanarq/CleanArchMvc/CleanArchMvc.Domain/Entities/Category.cs
@@ -1,11 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using CleanArchMvc.Domain.Validation;
+using System.Collections.Generic;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public class Category
+    // Não poderá ser herdada (selead)
+    public sealed class Category
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public ICollection<Product> Products { get; set; }
+        // Garantindo que os valores não vão ser atribuidos externamente
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public ICollection<Product> Products { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+            Name = name;
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characters");
+
+            Name = name;
+        }
     }
 }
